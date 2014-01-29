457233f11d6850307e7c1a721462146f73927832
yk-twww <good.yuk946@gmail.com>
2014-01-29 22:24:09 +0900

    implement most of E-step

--- src/main/scala/ihmm/optimizer.scala
+++ src/main/scala/ihmm/optimizer.scala
@@ -11,28 +11,64 @@ object Optimizer {
     stateN: Int): HMMparameter = {
     val param = new HMMparameter(Array(34.3), Array(Array(34.1)), Array(Map("hoge" -> 34.2)))
     return param
-    def BaumWelch(oldHMMparam: HMMparameter): HMMparameter = {
-      def calcLogLike(hmmParam: HMMparameter): Double = {
-        24324.4
+    def BaumWelch: HMMparameter = {
+      def calcLogLike(fbParams: List[FBparameter]): Double = {
+        fbParams.foldLeft(0.0) { (accumLogLike, fbParam) =>
+          fbParam.alphas.last.foldLeft(accumLogLike) { (_accumLogLike, alpha) =>
+            _accumLogLike + alpha
+          }
+        }
       }
-      def EStep(hmmParam: HMMparameter): FBParameter = {
-        "hoge"
+      def EStep(hmmParam: HMMparameter): List[FBParameter] = {
+        sentences.foldLeft(ListBuffer.empty[FBParameter]) { (fbParams, sentence) =>
+          val alphas = ListBuffer.empty[List[Double]] += (Range(0, stateN).toList.map { stateK =>
+            hmmParam.initProb(stateK) + hmmParam.emitProb(stateK)(sentence.head)
+          })
+          sentence.tail.foldLeft(alphas) { (_alphas, word) =>
+            _alphas += (Range(0, stateN).toList.map { stateK =>
+              val logProbs = _alphas.last.zipWithIndex.map { (_prob, _stateK) =>
+                _prob + hmmPram.transeProb(_stateK)(stateK)
+              }
+              logSumExp(logProbs) + hmmPram.emitProb(stateK)(word)
+            })
+          }
+          val betas = ListBuffer.empty[List[Double]] += (Range(0, stateN).toList.map(_ => 0.0))
+          sentence.tail.reverse.foldLeft(betas) { (_betas, word) =>
+            val beta = Range(0, stateN).toList.map { stateK =>
+              val logProbs = _betas.head.zipWithIndex.map { (_prob, _stateK) =>
+                _prob + hmmParam.emitProb(_stateK)(word) + hmmParam.transeProb(stateK)(_stateK)
+              }
+              logSumExp(logProbs)
+            }
+            beta +=: betas
+          }
+          new FBparameter(alphas, betas)
+        }
       }
       def MStep(param: FBparameter): HMMparameter = {
         "foo"
       }
-      def oneStep = (MStep compose EStep)
-      def _BaumWelch(oldHMMparam: HMMparameter, oldLoglike: Double): HMMparameter = {
-        val newHMMparam = oneStep(oldHMMparam)
-        val newLoglike  = calcLogLike(oldHMMparam)
+      def _BaumWelch(oldHMMparam: HMMparameter, oldLogLike: Double): HMMparameter = {
+        val newFBprams  = EStep(oldHMMparam)
+        val newHMMparam = MStep(newFBparams)
+        val newLoglike  = calcLogLike(newFBparams)
         if ((newLogLike - oldLogLike).abs < Threshold)
           newHMMparam
         else
           _BaumWelch(newHMMparam, newLogLike)
       }
-      val initialHMMparam = HMMparameter.randomInit(vocabulary, stateN)
-      val initialLogLike  = calcLogLike(initialHMMparam)
+      val initialHMMparam = HMMparamFactory.randomInit(vocabulary, stateN)
+      val initialFBparams = EStep(initialHMMparam)
+      val initialLogLike  = calcLogLike(initialFBparams)
       _BaumWelch(initialHMMparam, initialLogLike)
     }
+  }
+
+  def logSumExp(logProbs: List[Double]): Double = {
+    val maxLogProb = logProbs.max
+    val expSum = logProbs.foldLeft(0.0) { (expSum, logProb) =>
+      expSum + math.exp(logProb - maxLogProb)
+    }
+    maxLogProb + math.log(expSum)
   }
 }
--- src/main/scala/ihmm/parameter.scala
+++ src/main/scala/ihmm/parameter.scala
@@ -1,13 +1,25 @@
 package scala.ihmm
 
-class HMMparameter(_initProb: Array[Double],
-  _transeProb: Array[Array[Double]],
-  _emitProb: Array[Map[String, Double]]) {
+
+object HMMparamFactory {
+
+  def randomInit(vocabulary: List[String], stateN: Int): HMMparameter = {
+    val initProb:   Array[Double]
+    val transeProb: Array[Array[Double]]
+    val emitProb:   Array[Map[String, Double]]
+
+    return new HMMparameter(initProb, transeProb, emitProb)
+  }
+}
+
+class HMMparameter(_initProb: Array[Double], _transeProb: Array[Array[Double]], _emitProb: Array[Map[String, Double]]) {
   val initProb   = _initProb
   val transeProb = _transeProb
   val emitProb   = _emitProb
 }
 
-class FBparameter {
-  val hoge = "Hoge"
+// alphas[seq][state], betas[seq][state]
+class FBparameter(_alphas: Array[Array[Double]], _betas: Array[Array[Double]]) {
+  val alphas = _alphas
+  val betas  = _betas
 }
