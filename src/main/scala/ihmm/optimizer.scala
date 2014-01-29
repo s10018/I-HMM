3a211642da2668938680617d55a5c6df18400f4c
yk-twww <good.yuk946@gmail.com>
2014-01-30 00:24:58 +0900

    replace Array with ListBf

--- src/main/scala/ihmm/optimizer.scala
+++ src/main/scala/ihmm/optimizer.scala
@@ -1,27 +1,25 @@
 package scala.ihmm
 
-import collection.mutable.ListBuffer
+import collection.mutable.{ListBuffer => ListBf}
 
 
 object Optimizer {
 
   val Threshold = 0.001
 
-  def run(sentences: ListBuffer[Array[String]], vocabulary: List[String],
+  def run(sentences: ListBf[ListBf[String]], vocabulary: List[String],
     stateN: Int): HMMparameter = {
-    val param = new HMMparameter(Array(34.3), Array(Array(34.1)), Array(Map("hoge" -> 34.2)))
+    val param = new HMMparameter(ListBf(34.3), ListBf(ListBf(34.1)), ListBf(Map("hoge" -> 34.2)))
     return param
     def BaumWelch: HMMparameter = {
       def calcLogLike(fbParams: List[FBparameter]): Double = {
         fbParams.foldLeft(0.0) { (accumLogLike, fbParam) =>
-          fbParam.alphas.last.foldLeft(accumLogLike) { (_accumLogLike, alpha) =>
-            _accumLogLike + alpha
-          }
+          accumLogLike + fbParam.calcLogLike
         }
       }
       def EStep(hmmParam: HMMparameter): List[FBParameter] = {
-        sentences.foldLeft(ListBuffer.empty[FBParameter]) { (fbParams, sentence) =>
-          val alphas = ListBuffer.empty[List[Double]] += (Range(0, stateN).toList.map { stateK =>
+        sentences.foldLeft(ListBf.empty[FBParameter]) { (fbParams, sentence) =>
+          val alphas = ListBf.empty[List[Double]] += (Range(0, stateN).toList.map { stateK =>
             hmmParam.initProb(stateK) + hmmParam.emitProb(stateK)(sentence.head)
           })
           sentence.tail.foldLeft(alphas) { (_alphas, word) =>
@@ -32,7 +30,7 @@ object Optimizer {
               logSumExp(logProbs) + hmmPram.emitProb(stateK)(word)
             })
           }
-          val betas = ListBuffer.empty[List[Double]] += (Range(0, stateN).toList.map(_ => 0.0))
+          val betas = ListBf.empty[List[Double]] += (Range(0, stateN).toList.map(_ => 0.0))
           sentence.tail.reverse.foldLeft(betas) { (_betas, word) =>
             val beta = Range(0, stateN).toList.map { stateK =>
               val logProbs = _betas.head.zipWithIndex.map { (_prob, _stateK) =>
@@ -62,13 +60,5 @@ object Optimizer {
       val initialLogLike  = calcLogLike(initialFBparams)
       _BaumWelch(initialHMMparam, initialLogLike)
     }
-  }
-
-  def logSumExp(logProbs: List[Double]): Double = {
-    val maxLogProb = logProbs.max
-    val expSum = logProbs.foldLeft(0.0) { (expSum, logProb) =>
-      expSum + math.exp(logProb - maxLogProb)
-    }
-    maxLogProb + math.log(expSum)
   }
 }
