4d666f4a300f822b3d99e4d174b40ccbdca128e3
s_10018 <shiyuuzi99@gmail.com>
2014-01-30 15:53:59 +0900

    add randdir

--- src/test/scala/test_for_main.scala
+++ src/test/scala/test_for_main.scala
@@ -1,26 +1,10 @@
 
-package scala.plmrf
+package scala.ihmm
 
 import scala.collection.mutable.Stack
 import org.scalatest.FunSuite
 
 class PLMRF_Main_Test extends FunSuite {
-
-  test("ArgumentParse should get Map like below ver train") {
-    val opt1 = Main.ArgumentParse(Map(), List("train", "file"))
-    assert(opt1.contains("mode"))
-    assert(opt1.get("mode") == Some("train"))
-    assert(opt1.contains("inputfile"))
-    assert(opt1.get("inputfile") == Some("file"))
-  }
-
-  test("ArgumentParse should get Map like below ver decode") {
-    val opt2 = Main.ArgumentParse(Map(), List("decode", "file"))
-    assert(opt2.contains("mode"))
-    assert(opt2.get("mode") == Some("decode"))
-    assert(opt2.contains("inputfile"))
-    assert(opt2.get("inputfile") == Some("file"))
-  }
 
   test("ArgumentParse should get Map like below ver error type1") {
     val opt2 = Main.ArgumentParse(Map(), List())
@@ -31,11 +15,10 @@ class PLMRF_Main_Test extends FunSuite {
     val opt2 = Main.ArgumentParse(Map(), List("hoge", "hoge"))
     assert(opt2.contains("mode"))
     assert(opt2.get("mode") == Some(""))
-    assert(!opt2.contains("inputfile"))
   }
 
   test("Main should run main") {
-    expectResult()(scala.plmrf.Main.main(Array.empty))
+    expectResult()(Main.main(Array.empty))
   }
 
 }
