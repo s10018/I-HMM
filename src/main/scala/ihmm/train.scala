edcb0f720eaaa83646e463a6c77a929617882ce7
yk-twww <good.yuk946@gmail.com>
2014-02-05 00:31:17 +0900

    make reading train-file faster

--- src/main/scala/ihmm/train.scala
+++ src/main/scala/ihmm/train.scala
@@ -5,6 +5,9 @@ import java.io.PrintWriter
 import scala.annotation.tailrec
 import collection.mutable.{ListBuffer => ListBf}
 import collection.mutable.{Map => mMap}
+import collection.mutable.{ArrayBuffer => ArrayBf}
+
+
 
 object Train {
   val usage = "usage: jave -jar PL-MRF.jar train -train trainfile -layer layer_n -state state_n -c cut-off -dump dumpfile"
@@ -69,48 +72,50 @@ object Train {
   }
 
   def readAndSetData(testPath: String, cutOff: Int): (Array[Array[String]], Array[String]) = {
-    def convert2sentences(testPath: String): Array[Array[String]] = {
-      def split2words(sentence: String): Array[String] = {
-        sentence.split(" ")
-      }
-      val s = Source.fromFile(testPath)
-      val sentences = ListBf.empty[Array[String]]
-
-      s.getLines.foreach { line => sentences += split2words(line) }
-      s.close
-      sentences.toArray
-    }
-    def countFreq(sentences: Array[Array[String]]): mMap[String, Int] = {
-      sentences.iterator.foldLeft(mMap.empty[String, Int]) { (mapC, sentence) =>
-        sentence.iterator.foldLeft(mapC) { (_mapC, word) =>
-          _mapC + (word -> (_mapC.getOrElse(word, 0) + 1))
-        }
-      }
-    }
-    def extractLowFreqWord(wordFreq: mMap[String, Int]): mMap[String, Int] = {
-      wordFreq.filter(wdCount => wdCount._2 <= cutOff)
-    }
-    def excludeLowFreqWord(sentences: Array[Array[String]], lowFreqWord: mMap[String, Int]): Unit = {
-      sentences.iterator.foreach { sentence =>
-        Range(0, sentence.size).iterator.foreach { k =>
-          sentence(k) = if (lowFreqWord.contains(sentence(k))) UNKNOWN else sentence(k)
-        }
-      }
-    }
-    def examinVocabulary(wordFreq: mMap[String, Int], lowFreqWord: mMap[String, Int]): Array[String] = {
-      val preVocabulary = wordFreq.filter { wdCount => wdCount._2 > cutOff }
-      if (lowFreqWord.size == 0) {
-        preVocabulary.keys.toArray
-      } else {
-        UNKNOWN +: preVocabulary.keys.toArray
-      }
-    }
     val sentences   = convert2sentences(testPath)
     val wordFreq    = countFreq(sentences)
-    val lowFreqWord = extractLowFreqWord(wordFreq)
-    val vocabulary  = examinVocabulary(wordFreq, lowFreqWord)
+    val lowFreqWord = extractLowFreqWord(wordFreq, cutOff)
+    val vocabulary  = examinVocabulary(wordFreq, lowFreqWord, cutOff)
     excludeLowFreqWord(sentences, lowFreqWord)
 
     (sentences, vocabulary)
+  }
+  def convert2sentences(testPath: String): Array[Array[String]] = {
+    val s         = Source.fromFile(testPath)
+    val sentences = ListBf.empty[Array[String]]
+
+    s.getLines.foreach { line => sentences += split2words(line) }
+    s.close
+    sentences.toArray
+  }
+  def split2words(sentence: String): Array[String] = {
+    sentence.split(" ")
+  }
+  def countFreq(sentences: Array[Array[String]]): mMap[String, Int] = {
+    val wordFreq = mMap.empty[String, Int]
+    sentences.iterator.foreach { sentence =>
+      sentence.iterator.foreach { word =>
+        wordFreq(word) = wordFreq.getOrElse(word, 0) + 1
+      }
+    }
+    wordFreq
+  }
+  def extractLowFreqWord(wordFreq: mMap[String, Int], cutOff: Int): mMap[String, Int] = {
+    wordFreq.filter(wdCount => wdCount._2 <= cutOff)
+  }
+  def excludeLowFreqWord(sentences: Array[Array[String]], lowFreqWord: mMap[String, Int]): Unit = {
+    sentences.iterator.foreach { sentence =>
+      Range(0, sentence.size).iterator.foreach { k =>
+        sentence(k) = if (lowFreqWord.contains(sentence(k))) UNKNOWN else sentence(k)
+      }
+    }
+  }
+  def examinVocabulary(wordFreq: mMap[String, Int], lowFreqWord: mMap[String, Int], cutOff: Int): Array[String] = {
+    val preVocabulary = wordFreq.filter { wdCount => wdCount._2 > cutOff }
+    if (lowFreqWord.size == 0) {
+      preVocabulary.keys.toArray
+    } else {
+      UNKNOWN +: preVocabulary.keys.toArray
+    }
   }
 }
