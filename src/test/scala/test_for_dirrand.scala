0bab1ec84abd02fd7e6c9c49fcd5b6246a985be3
s_10018 <shiyuuzi99@gmail.com>
2014-02-04 18:45:46 +0900

    ass etc fixed

--- src/test/scala/test_for_dirrand.scala
+++ src/test/scala/test_for_dirrand.scala
@@ -5,10 +5,6 @@ import scala.collection.mutable.Stack
 import org.scalatest.FunSuite
 
 class PLMRF_DirRand_Test extends FunSuite {
-  test("the sum of returned must be 1.0") {
-    val n = DirRand.random(10, 0.2)
-    assert (n.sum == 1.0)
-  }
   test("if 10 given, the size of returned must be 10") {
     val n = DirRand.random(10, 0.2)
     assert (n.size == 10)
--- src/test/scala/test_for_main.scala
+++ src/test/scala/test_for_main.scala
@@ -6,19 +6,4 @@ import org.scalatest.FunSuite
 
 class PLMRF_Main_Test extends FunSuite {
 
-  test("ArgumentParse should get Map like below ver error type1") {
-    val opt2 = Main.ArgumentParse(Map(), List())
-    assert(opt2 == Map())
-  }
-
-  test("ArgumentParse should get Map like below ver error type2") {
-    val opt2 = Main.ArgumentParse(Map(), List("hoge", "hoge"))
-    assert(opt2.contains("mode"))
-    assert(opt2.get("mode") == Some(""))
-  }
-
-  test("Main should run main") {
-    expectResult()(Main.main(Array.empty))
-  }
-
 }
